32047d5245096a3bfe5a82cf7ac2ca9353cc41b3
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2023-02-13 20:28:05 -0600

    kl

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import {FaSearch} from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
+import { useGlobalContext } from '../context.';
 
 const SearchForm = () => {
+  const {setSearchTerm, setResultTitle} = useGlobalContext();
+  const searchText = useRef('');
+  const navigate = useNavigate();
+
+  useEffect(()=> searchText.current.focus(), []);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let tempSearchTerm = searchText.current.value.trim();
+    if((tempSearchTerm.replace(/[^\w3\s]/gi,"")).length === 0) {
+      setSearchTerm("the lost world");
+      setResultTitle("Please Enter Something...");
+    } else {
+      setSearchTerm(searchText.current.value);
+    }
+
+    navigate("/book");
+  }
   return (
     <div className='w-screen flex p-12 items-center justify-center '>
-          <form className='bg-white w-[480px] flex justify-between p-2 rounded-lg'>
-              <input type="text" placeholder="the Last Book..." className='font-bold uppercase ont-poppins w-full bg-transparent focus:outline-none text-[rgb(136,152,185)]' />
-              <button>
+          <form onSubmit={handleSubmit} className='bg-white w-[480px] flex justify-between p-2 rounded-lg'>
+              <input type="text" ref={searchText} placeholder="the Last Book..." className='font-bold uppercase ont-poppins w-full bg-transparent focus:outline-none text-[rgb(136,152,185)]' />
+              <button onClick={handleSubmit} type="submit">
                 <FaSearch size={24} className="text-[rgb(136,152,185)]"/>
               </button>
           </form>
--- src/context..jsx
+++ src/context..jsx
@@ -15,7 +15,32 @@ const AppProvider = ({children}) => {
             const response = await fetch(`${URL}${searchTerm}`);
             const data = await response.json();
             const {docs} = data;
-            console.log(docs)
+            console.log(docs);
+
+            if(docs){
+                const newBooks = docs.slice(0,20).map((bookSingle) => {
+                    const {key, author_name, cover_i, edition_count, first_publish_year, title} = bookSingle;
+
+                return {
+                    id: key,
+                    author: author_name,
+                    cover_id: cover_i,
+                    edition_count: edition_count,
+                    first_publish_year: first_publish_year,
+                    title: title
+                }
+                });
+
+                setBooks(newBooks);
+
+                if(newBooks.length > 1){
+                    setResultTitle("Your Search Result");
+
+                } else {
+                    setResultTitle("No Search Result Found");
+                } 
+                setLoading(false);
+            }
         } catch (error){
             console.log(error);
             setLoading(false);
