ae4b4f0f1662458c5b0f8d19c4805cc4a25c8ef4
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2023-02-19 15:40:05 -0600

    hero

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -23,8 +23,8 @@ const SearchForm = () => {
     // navigate("/book");
   }
   return (
-    <div className='w-screen flex p-12 items-center justify-center '>
-    <form onSubmit={handleSubmit} className='bg-slate-100 w-[480px] flex justify-between p-2 rounded-lg'>
+    <div className='w-screen flex py-12 items-center justify-center '>
+    <form onSubmit={handleSubmit} className='bg-slate-100 w-3/6 flex justify-between p-4 rounded-lg'>
               <input type="text" ref={searchText} placeholder="the Lost World..." className='font-bold uppercase ont-poppins w-full bg-transparent focus:outline-none text-[rgb(136,152,185)]' />
               <button onClick={handleSubmit} type="submit">
                 <FaSearch size={24} className="text-[rgb(136,152,185)]"/>
