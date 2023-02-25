245ddd3ae214eccf91fbbc55f629b028fb0e67d9
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2023-02-25 17:41:48 -0600

    footer bg

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div className='w-full h-[250px] mt-24 clip-path-footercurve bg-[rgb(255,136,106)] flex items-center text-center justify-between flex-row px-2 sm:px-24'>
+    <div className='w-full h-[250px] mt-24 clip-path-footercurve bg-white flex items-center text-center justify-between flex-row px-2 sm:px-24'>
         <span className='font-logo uppercase font-bold text-xl text-[rgb(136,152,185)] '>Faith Nguyen 2023</span>
         <span className='font-oswald text-[rgb(136,152,185)] text-xl '>Come Visit
           <div className="flex flex-row gap-4 text-center uppercase text-sm md:text-md font-bold text-white ">
